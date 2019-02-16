2c95317458de1518264026c8169063fa2f83960d
DmrLds <dmr.lutfullin@gmail.com>
2019-02-16 21:28:40 +0500

    Новые классы. Подготовка к движению змейки

--- SnakeVS/SnakeVS/Program.cs
+++ SnakeVS/SnakeVS/Program.cs
@@ -26,10 +26,10 @@ namespace SnakeVS
             Point p = new Point(4,5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Drow();
+            snake.Move();
 
             Console.ReadLine();
-
-
+            
         }
         
     }
